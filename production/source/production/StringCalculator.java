d2f851286a7c6d273392a7409d5ba96056360ee8
Federico Camerota <fedes770@gmail.com>
2019-11-14 20:49:59 +0100

    Refactoring add method by method extraction

--- production/source/production/StringCalculator.java
+++ production/source/production/StringCalculator.java
@@ -6,33 +6,46 @@ import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class StringCalculator  {
+
+    private static IntStream parseInputString(String inputString){
+
+            String separator = "[,\\n]";
+            if (inputString.startsWith("//")) {
+                int firstNewLine = inputString.indexOf('\n');
+                separator = inputString.substring(2,firstNewLine);
+                inputString = inputString.substring(firstNewLine + 1);
+            }
+
+            Pattern splitPattern = Pattern.compile(separator);
+
+        return splitPattern.splitAsStream(inputString.trim()).mapToInt(Integer::valueOf);
+    }
+
+    private static String negativesExceptionMessage(List<Integer> negatives){
+
+        StringBuilder exceptionText = new StringBuilder("Negatives not allowed: ");
+        for ( Integer negativeNumber : negatives) {
+            exceptionText.append(negativeNumber).append(" ");
+        }
+
+        return exceptionText.toString().trim();
+    }
+
     public static int add(String numbers) throws StringFormatException {
 
         if (numbers.isEmpty())
             return 0;
         else {
 
-            String separator = "[,\\n]";
-            if (numbers.startsWith("//")) {
-                int firstNewLine = numbers.indexOf('\n');
-                separator = numbers.substring(2,firstNewLine);
-                numbers = numbers.substring(firstNewLine + 1);
-            }
-            Pattern splitPattern = Pattern.compile(separator);
-            IntStream numStream = splitPattern.splitAsStream(numbers.trim()).mapToInt(Integer::valueOf);
+            IntStream numStream = parseInputString(numbers);
 
             List<Integer> negatives = new LinkedList<Integer>();
 
             int sum = numStream.map(x -> {if (x < 0) negatives.add(x); return x;}).sum();
 
-            if (negatives.size() > 0){
+            if (negatives.size() > 0)
+                throw new StringFormatException(negativesExceptionMessage(negatives));
 
-                StringBuilder exceptionText = new StringBuilder("Negatives not allowed: ");
-                for ( Integer negativeNumber : negatives) {
-                    exceptionText.append(negativeNumber).append(" ");
-                }
-                throw new StringFormatException(exceptionText.toString().trim());
-            }
             return sum;
         }
 
